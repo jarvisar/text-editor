0b087c466f9334e4263914baf91e270683a88def
jarvisar <jarvisar@mail.uc.edu>
2023-03-09 09:54:45 -0500

    Remove debug print statement

--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -63,7 +63,7 @@ namespace TextEditor
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Form loaded!");
+            
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
